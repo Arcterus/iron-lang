8bdb03110042b0270671746656c2afde51628077
Manan Shah <manan.shah.777@gmail.com>
2014-05-12 17:43:30 -0700

    Update parser.rs
    More fixes

--- src/parser.rs
+++ src/parser.rs
@@ -114,7 +114,7 @@ impl Parser {
 			}
 			Ok(box SexprAst::new(op, FromVec::from_vec(operands)) as Box<Ast>)
 		} else {
-			Err(self.expect_error("'('", format!("'{}'", self.code.char_at(self.pos))))
+			Err(self.unexpected_error("'('", format!("'{}'", self.code.char_at(self.pos))))
 		}
 	}
 
@@ -142,7 +142,7 @@ impl Parser {
 			self.inc_pos_col();
 		}
 		if digits == 0 {
-			Err(self.expect_error("integer", format!("'{}'", self.code.char_at(self.pos))))
+			Err(self.unexpected_error("integer", format!("'{}'", self.code.char_at(self.pos))))
 		} else {
 			Ok((if neg { -number } else { number }, digits))
 		}
@@ -153,11 +153,11 @@ impl Parser {
 		if self.pos + 1 >= self.code.len() {
 			Err(self.eof_error())
 		} else if self.code.char_at(self.pos) != '.' {
-			Err(self.expect_error("'.'", format!("'{}'", self.code.char_at(self.pos))))
+			Err(self.unexpected_error("'.'", format!("'{}'", self.code.char_at(self.pos))))
 		} else {
 			self.inc_pos_col();
 			if !self.code.char_at(self.pos).is_digit() {
-				Err(self.expect_error("float", format!("'{}'", self.code.char_at(self.pos))))
+				Err(self.unexpected_error("float", format!("'{}'", self.code.char_at(self.pos))))
 			} else {
 				let back = try!(self.parse_integer_val());
 				Ok(box FloatAst::new(front as f64 + back.val0() as f64 / num::pow(10, back.val1()) as f64) as Box<Ast>)
@@ -185,14 +185,14 @@ impl Parser {
 						if ident.len() > 0 {
 							ident.push_char(num);
 						} else {
-							return Err(self.expect_error("ident", format!("'{}'", num)));
+							return Err(self.unexpected_error("ident", format!("'{}'", num)));
 						}
 					}
 					other => {
 						if ident.len() > 0 {
 							break
 						} else {
-							return Err(self.expect_error("ident", format!("'{}'", other)));
+							return Err(self.unexpected_error("ident", format!("'{}'", other)));
 						}
 					}
 				};
@@ -228,7 +228,7 @@ impl Parser {
 				Ok(box StringAst::new(buf.into_owned()) as Box<Ast>)
 			}
 		} else {
-			Err(self.expect_error("\"", format!("'{}'", self.code.char_at(self.pos))))
+			Err(self.unexpected_error("\"", format!("'{}'", self.code.char_at(self.pos))))
 		}
 	}
 
